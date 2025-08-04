b26cf18b9709a85c1707ebf7976c96b40534e8f7
Daniel Zamora Paez <[email>
2025-08-04 14:16:27 -0500

    Cambios funcionalidad

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const buscador = document.getElementById('nombre');
 const url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+const nombreBuscar = document.getElementById('inputBuscador').value;
 buscador.addEventListener('click', function(){
     fetch(url).then(function(response) {
         return response.json();
     })
     .then(function(data) {
         // Datos del Pokémon aquíconsole.log(data);
+        aquíconsole.log(data);
     })
     .catch(function(error) {alert("¡Error! Pokémon no encontrado");});
 
